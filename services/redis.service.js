066cc1127f0622f7a84ae434147ef87319b911b0
semdantani <prabhasdantani432@gmail.com>
2025-08-28 12:22:44 +0530

    Update redis.service.js

--- services/redis.service.js
+++ services/redis.service.js
@@ -4,9 +4,12 @@ const redisClient = new Redis({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT || 6379,
   password: process.env.REDIS_PASSWORD,
+  tls: {},
 });
 
-redisClient.on("connect", () => {});
+redisClient.on("connect", () => {
+  console.log("Redis connected");
+});
 
 redisClient.on("error", (err) => {
   console.error("Redis connection error:", err);
