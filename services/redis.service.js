cf4f28286f052e78c64e28f9584e03cba37e0470
semdantani <prabhasdantani432@gmail.com>
2025-08-28 13:21:04 +0530

    Update redis.service.js

--- services/redis.service.js
+++ services/redis.service.js
@@ -2,4 +2,12 @@ import Redis from "ioredis";
 
 const redisClient = new Redis(process.env.REDIS_URL);
 
+redisClient.on("connect", () => {
+  console.log("✅ Redis connected");
+});
+
+redisClient.on("error", (err) => {
+  console.error("❌ Redis error:", err);
+});
+
 export default redisClient;
